8e12eaaa19259536d8001ed94d19afdbd341af0b
luis angel ponce alvarez <poncealvarezluisangel4@gmail.com>
2023-03-04 16:32:40 -0600

    configuración de certificados

--- assets/js/menu.js
+++
@@ -1,7 +0,0 @@
-const btnmenu = document.querySelector("#btnmenu");
-const menu = document.querySelector("#menu");
-
-btnmenu.addEventListener("click", () => {
-  menu.classList.toggle("mostrar");
-});
-
